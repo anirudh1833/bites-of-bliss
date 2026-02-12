db78aed3d451fb4eacabeaee87b1eda2cd2694f3
Anirudh Muralidharan <68141146+anirudh1833@users.noreply.github.com>
2026-02-12 10:25:47 +0000

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,8 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import MeetBaker from "@/components/MeetBaker";
 import Menu from "@/components/Menu"; // Import the new Menu component
+import Testimonials from "@/components/Testimonials";
+import FAQ from "@/components/FAQ";
 import CustomCakeForm from "@/components/CustomCakeForm";
 import Footer from "@/components/Footer";
 import FloatingWhatsApp from "@/components/FloatingWhatsApp";
@@ -14,6 +16,8 @@ const Index = () => {
         <Hero />
         <MeetBaker />
         <Menu /> {/* Used to be Gallery */}
+        <Testimonials />
+        <FAQ />
         <CustomCakeForm />
       </main>
       <Footer />
